567054595a8ff4c77de036006be4936848092ae4
Tan Kai <6572withspicy@gmail.com>
2024-08-08 23:32:48 +0800

    refactor: Move some of the responsibilities to its own function

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/tantanok221/cloudflare-image-backend/db"
 	"github.com/tantanok221/cloudflare-image-backend/internal/helper"
 	"github.com/tantanok221/cloudflare-image-backend/utils"
@@ -13,6 +15,12 @@ import (
 	"net/http"
 	"time"
 )
+
+type cloudflareResponse *struct {
+	Result struct {
+		Variants []string `json:"variants"`
+	} `json:"result"`
+}
 
 func PostImage() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -25,7 +33,7 @@ func PostImage() http.HandlerFunc {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		defer image.Close()
+		image.Close()
 		contentType := header.Header.Get("Content-Type")
 		if !utils.IsImage(contentType) {
 			http.Error(w, http.StatusText(http.StatusUnsupportedMediaType), http.StatusUnsupportedMediaType)
@@ -37,49 +45,18 @@ func PostImage() http.HandlerFunc {
 			return
 		}
 
-		body := &bytes.Buffer{}
-		writer := multipart.NewWriter(body)
-		part, err := writer.CreateFormFile("file", header.Filename)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		_, err = io.Copy(part, image)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		err = writer.Close()
-		ir, err := http.NewRequest("POST", utils.GetEndpoint(), body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		client := &http.Client{}
+		body, writer, err := WriteFormData(image, header)
 
-		ir.Header.Set("Content-Type", writer.FormDataContentType())
-		ir.Header.Set("Authorization", utils.GetAuthToken())
-
-		res, err := client.Do(ir)
 		if err != nil {
-			http.Error(w, "Error on uploading to cloudflare image"+err.Error(), http.StatusInternalServerError)
-		}
-		defer res.Body.Close()
-		var cloudflareResponse struct {
-			Result struct {
-				Variants []string `json:"variants"`
-			} `json:"result"`
-		}
-		if err := json.NewDecoder(res.Body).Decode(&cloudflareResponse); err != nil {
-			http.Error(w, "Error decoding Cloudflare response: "+err.Error(), http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-
-		if len(cloudflareResponse.Result.Variants) == 0 {
-			http.Error(w, "No image variants returned from Cloudflare", http.StatusInternalServerError)
+		response, err := ReturnCloudflareImageResponse(body, writer)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		imageLink := cloudflareResponse.Result.Variants[0]
+		imageLink := response.Result.Variants[0]
 		data := utils.Path{
 			ImageLink: imageLink,
 			Name:      name,
@@ -91,4 +68,53 @@ func PostImage() http.HandlerFunc {
 			http.Error(w, "Insert MongoDB went wrong: \n"+err.Error(), http.StatusInternalServerError)
 		}
 	}
+}
+
+func WriteFormData(image multipart.File, header *multipart.FileHeader) (*bytes.Buffer, *multipart.Writer, error) {
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("file", header.Filename)
+	print("Writing Form data \n")
+	if err != nil {
+		return nil, writer, err
+	}
+	_, err = io.Copy(part, image)
+	if err != nil {
+		return nil, writer, err
+	}
+	err = writer.Close()
+	return body, writer, nil
+}
+
+func PostCloudflareImageAPI(body *bytes.Buffer, writer *multipart.Writer) (*http.Response, error) {
+	ir, err := http.NewRequest("POST", utils.GetEndpoint(), body)
+	print("Posting API Data \n")
+	if err != nil {
+		return nil, err
+	}
+	client := &http.Client{}
+
+	ir.Header.Set("Content-Type", writer.FormDataContentType())
+	ir.Header.Set("Authorization", utils.GetAuthToken())
+	fmt.Printf("%v", ir.Header)
+	res, err := client.Do(ir)
+	return res, err
+}
+
+func ReturnCloudflareImageResponse(body *bytes.Buffer, writer *multipart.Writer) (cloudflareResponse, error) {
+	res, err := PostCloudflareImageAPI(body, writer)
+	var response cloudflareResponse
+	if err != nil {
+		return nil, errors.New("Error on uploading to cloudflare image" + err.Error())
+	}
+	defer res.Body.Close()
+	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+		return nil, errors.New("Error decoding Cloudflare response: " + err.Error())
+	}
+
+	if len(response.Result.Variants) == 0 {
+		return nil, errors.New("No image variants returned from Cloudflare")
+	}
+	err = writer.Close()
+	return response, nil
 }
